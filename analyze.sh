86b7af7ecdbf8149791d6ccec24b4307245f8c34
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-05-08 17:30:07 +0300

    Fixed analyzer start parameters

--- analyze.sh
+++ analyze.sh
@@ -6,5 +6,5 @@
 /usr/bin/env python3 src/analyzer.py \
         --out_file $2 --batch_size 5 --learning_rate 0.1 \
         --contraction_level 0.1 --corruption_level 0.1 \
-        --n_hidden 16 --training_epochs 10000 \
+        --n_hidden 16 --training_epochs 50000 \
         --encoder cA $1 --save_plot True
