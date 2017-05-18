ee26adb3f9cece76f066a496c4bdccd46a63c8c3
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-05-18 17:23:32 +0300

    The best parameters for two bands filter

--- analyze.sh
+++ analyze.sh
@@ -6,5 +6,6 @@
 /usr/bin/env python3 src/analyzer.py \
         --out_file $2 --batch_size 5 --learning_rate 0.1 \
         --contraction_level 0.1 --corruption_level 0.3 \
-        --n_hidden 7 --training_epochs 50000 \
-        --encoder cA $1 --save_plot True
+        --n_hidden 2 --training_epochs 50000 \
+        --encoder cA $1 --save_plot True \
+        --bands thetha,alpha_h
