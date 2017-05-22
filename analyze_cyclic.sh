9cd0af56e37ca773b2cd37c042d9e57efdf04a7b
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-05-22 14:12:32 +0300

    Added command line argument to define start range of files to process

--- analyze_cyclic.sh
+++ analyze_cyclic.sh
@@ -3,7 +3,14 @@
 # The analyze cyclic
 #
 
-for i in `seq 1 $2`;
+if [ "$#" -ne 3 ]; then
+    START=1
+else
+    START=$3
+fi
+
+
+for i in `seq $START $2`;
 do
     ./analyze.sh out/intermediate/$1/preprocessed_$i.npy out/analyzer/$1/$1_$i.npy
 done 
