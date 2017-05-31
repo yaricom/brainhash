d497f7cac53e205efb4a3c4438ae976ee0c00f00
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-05-31 18:32:49 +0300

    Started experiment runner implementation

---
+++ experiment.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# The experiment runner
+#
+#
+#
+
+# The experiment parameters 
+learning_rate=0.1
+n_hidden=2
+training_epochs=50000
+bands=delta,alpha_h
+batch_size=5
+
+run_analyzer()
+
+
