285ac31a1c67c0255229ef3112483306fd33dc51
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-06-08 18:05:04 +0300

    Fixed CML hyper parameters

--- analyze.sh
+++ analyze.sh
@@ -20,8 +20,13 @@ if [[ "$#" -lt 2 ]]; then
 fi
 
 /usr/bin/env python3 src/analyzer.py \
-        --out_file $2 --batch_size 5 --learning_rate 0.1 \
-        --contraction_level 0.1 --corruption_level 0.3 \
-        --n_hidden 2 --training_epochs 50000 \
-        --encoder cA $1 --save_plot True \
-        --bands delta,alpha_h
+        --out_file $2 \
+        --batch_size 1 \
+        --learning_rate 0.1 \
+        --contraction_level 0.1 \
+        --corruption_level 0.3 \
+        --n_hidden 3\
+        --training_epochs 50000 \
+        --encoder cA $1 \
+        --save_plot True \
+        --bands delta,theta,alpha_l,alpha_h
